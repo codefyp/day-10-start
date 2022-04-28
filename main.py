a9aaeea30745038c04bfd85b8eac7577b56db3ca
codefyp <idivertor@gmail.com>
2022-04-28 17:20:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    #1 print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}" #return is the end of the function and nothing can be executed after return
+#1 format_name("angelo", "JELO")
+
+#2 formated_string = format_name("angelo", "JELO")
+print(format_name("angelo", "JELO")) #2#3
